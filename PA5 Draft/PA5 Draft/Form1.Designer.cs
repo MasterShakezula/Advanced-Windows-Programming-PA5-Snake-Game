0439b3910c46e0fd02c0e4d88efc261b24c2e245
MasterShakezula <90061079+MasterShakezula@users.noreply.github.com>
2022-10-31 11:35:54 -0400

    Initial commit

---
+++ PA5 Draft/PA5 Draft/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PA5_Draft
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Field = new System.Windows.Forms.PictureBox();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Field
+            // 
+            this.Field.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Field.Location = new System.Drawing.Point(0, 0);
+            this.Field.Name = "Field";
+            this.Field.Size = new System.Drawing.Size(604, 581);
+            this.Field.TabIndex = 0;
+            this.Field.TabStop = false;
+            this.Field.Paint += new System.Windows.Forms.PaintEventHandler(this.Field_Paint);
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Enabled = true;
+            this.mainTimer.Interval = 12;
+            this.mainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 581);
+            this.Controls.Add(this.Field);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(620, 620);
+            this.MinimumSize = new System.Drawing.Size(620, 620);
+            this.Name = "MainForm";
+            this.Text = "Snakes";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Snakes_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Field;
+        private System.Windows.Forms.Timer mainTimer;
+    }
+}
+
